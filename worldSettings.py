aedb06ddd0d804c4b49ce7eecb3092f9c7e5caf6
Murali Krishna Lingamsetty <murali.lingamsetty@outlook.com>
2024-12-03 13:31:19 -0500

    Moved textures to worldSettings

--- worldSettings.py
+++ worldSettings.py
@@ -14,7 +14,39 @@ class worldSettings:
         self.player_speed = 8                                                       # Player movement speed
         self.home_size = 30                                                         # Home size x Home size
         self.home_min_z = self.world_size - 80                                      # Home min z position
-        self.home_max_z = self.world_size - 50                                      # Home max z position
+        self.home_max_z = self.world_size - 50     
+        
+        from ursina import *
+        
+        # Load block textures
+        self.block_textures = {
+            "grass": load_texture("minecraft_starter/assets/textures/groundEarth.png"),
+            "dirt": load_texture("minecraft_starter/assets/textures/groundMud.png"),
+            "stone": load_texture("minecraft_starter/assets/textures/Stone01.png"),
+            "bedrock": load_texture("minecraft_starter/assets/textures/stone07.png"),
+            "lava": load_texture("minecraft_starter/assets/textures/lava01.png"),
+            "water": load_texture("minecraft_starter/assets/textures/water.png"),
+            "torch" : load_texture("minecraft_starter/assets/textures/Diffuse.png"),
+            "obstacleTile" : load_texture("minecraft_starter/assets/textures/wallBrick05.png"),
+            "wood" : load_texture("minecraft_starter/assets/textures/Wood.png"),
+            "leaves" : load_texture("minecraft_starter/assets/textures/leaf.png"),
+            "wall" : load_texture("minecraft_starter/assets/textures/wallStone.png"),
+            "darkstone" : load_texture("minecraft_starter/assets/textures/stone02.png"),
+            "mud" : load_texture("minecraft_starter/assets/textures/groundMud.png"),
+            "trimmedGrass" : load_texture("minecraft_starter/assets/textures/stone06.png"),
+            "snow" : load_texture("minecraft_starter/assets/textures/snow.png"), #BFS and DFS
+            # Add other block textures if needed
+        }
+
+        self.mob_textures = {
+            "zombie" : load_texture("minecraft_starter/assets/textures/zombie.png"),
+            "enderman" : load_texture("minecraft_starter/assets/textures/enderman.png"),
+            "creeper" : load_texture("minecraft_starter/assets/textures/creeper.png"),
+            "skeleton" : load_texture("minecraft_starter/assets/textures/skeleton.png"),
+            # Add other mob textures if needed
+        }
+
+    # Home max z position
 
     def get_world_size(self):
         return self.world_size
